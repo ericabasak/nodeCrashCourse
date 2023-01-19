a5f380237c4e04975d722271dbb3e9634aa623a6
Erica Basak <ericabasak@gmail.com>
2023-01-19 00:22:47 -0500

    continued implement middleware and get request

---
+++ members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: "erica basak",
+        email: "erica@gmail.com",
+        status: "active"
+    },
+    {
+        id: 2,
+        name: "adelyn basak",
+        email: "adelyn@gmail.com",
+        status: "active"
+    },
+    {
+        id: 3,
+        name: "asher basak",
+        email: "asher@gmail.com",
+        status: "inactive"
+    }
+]
+
+module.exports = members;
