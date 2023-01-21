c704f986ec443ec2e5ecf84765c7c67e6c092909
Erica Basak <ericabasak@gmail.com>
2023-01-21 15:07:44 -0500

    crud functionality with api

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const path = require('path');
 const members = require('./members');
-const member = require('./members')
 const logger = require('./middleware/logger')
 
 const app = express();
@@ -10,32 +9,15 @@ const app = express();
 // init middleware
 // app.use(logger);
 
-// this route gets all members
-app.get('/api/members', (req, res) => res.json(members));
-
-// get single member
-app.get('/api/members/:id', (req, res) =>{
-    // lets us know if a member is found with that same id
-    // high order array method
-    const found = members.some(member => member.id === parseInt(req.params.id))
-
-    if(found) {
-        // res.send(req.params.id)
-        res.json(members.filter(member => member.id === parseInt(req.params.id)))
-    } else {
-        res.status(400).json({ message: `No member with the id of ${req.params.id} found`});
-    }
-    
-})
-
-// // create a route below
-// app.get("/", (req, res) => {
-//     // res.send('<h1>Hello world!!</h1>')
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+// body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // set static folder
 app.use(express.static(path.join(__dirname, "public")))
+
+// memnbers api routes
+app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 5000;
 
@@ -43,4 +25,3 @@ app.listen(PORT, () => {
     console.log(`Server starting on port: ${PORT}`);
 });
 
-// the above lines of code will start the server!!
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const member = require('../../members');
+const uuid = require('uuid');
+const members = require('../../members');
+const { update } = require('tar');
 
 // this route gets all members
-app.get('/api/members', (req, res) => res.json(members));
+router.get('/', (req, res) => res.json(members));
 
 // get single member
-app.get('/api/members/:id', (req, res) =>{
+router.get('/:id', (req, res) =>{
     // lets us know if a member is found with that same id
     // high order array method
     const found = members.some(member => member.id === parseInt(req.params.id))
@@ -17,5 +20,64 @@ app.get('/api/members/:id', (req, res) =>{
     } else {
         res.status(400).json({ message: `No member with the id of ${req.params.id} found`});
     }
-    
 })
+
+// create member
+router.post('/', (req, res) => {
+    // res.send(req.bo{dy)
+    const newMember = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email,
+        status: "active",
+    }
+
+    if(!newMember.name || !newMember.email) {
+        return res.status(400).json({ message: "please fill out name and emails" });
+    }
+
+    // use save when saving to a database
+    members.save(newMember)
+    // below is for hard coded data
+    members.push(newMember);
+    res.json(members);
+})
+
+// update member
+router.put('/:id', (req, res) =>{
+    // lets us know if a member is found with that same id
+    // high order array method
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if(found) {
+        const updateMember = req.body;
+        members.forEach(member => {
+            if(member.id === parseInt(req.body.id)) {
+                member.name === updateMember.name ? updateMember.name : member.name;
+                member.email === updateMember.email ? updateMember.email : member.email;
+
+                res.json({ message: "member updated", member });
+            }
+        })
+    } else {
+        res.status(400).json({ message: `No member with the id of ${req.params.id} found`});
+    }
+})
+
+// delete member
+router.delete('/:id', (req, res) =>{
+    // lets us know if a member is found with that same id
+    // high order array method
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if(found) {
+        // res.send(req.params.id)
+        res.json({ 
+            message: "member deleted", 
+            members: members.filter(member => member.id === parseInt(req.params.id))})
+    } else {
+        res.status(400).json({ message: `No member with the id of ${req.params.id} found`});
+    }
+})
+
+module.exports = router;
